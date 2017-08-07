b0495e88154ce128f44df385edac252c1ddcaf7f
Jurgis Bridzius <jurgis@bridzius.lt>
2017-08-07 22:03:55 +0300

    [Dev] - Added default config file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var path = require("path");
+var fs = require("fs");
 var protractorPath = path.dirname(require.resolve("protractor"));
 var launcherPath = protractorPath + "/launcher";
 var launcher = require(launcherPath);
 var configFile = fs.existsSync(path.join(process.cwd(), "protractor.conf.js")) && path.join(process.cwd(), "protractor.conf.js");
 if (!configFile) {
-    if (fs.existsSync('./protractor.conf.js')) {
-        configFile = './protractor.conf.js';
+    if (fs.existsSync(path.join(__dirname, 'protractor.conf.js'))) {
+        configFile = path.join(__dirname, 'protractor.conf.js');
     }
 }
 
