2de006abff8d192d1d8a63f0d17d2507f05f8672
Jurgis Bridzius <jurgis@bridzius.lt>
2017-08-07 16:00:29 +0300

    Added access for default arguments

---
+++ index.js
@@ -0,0 +1,15 @@
+var path = require("path");
+var protractorPath = path.dirname(require.resolve("protractor"));
+var launcherPath = protractorPath + "/launcher";
+var launcher = require(launcherPath);
+var configFile = fs.existsSync(path.join(process.cwd(), "protractor.conf.js"));
+if (!configFile) {
+    if (fs.existsSync('./protractor.conf.js')) {
+        configFile = './protractor.conf.js';
+    }
+}
+launcher.init(configFile);
+
+module.exports = function(argumentsFile) {
+  launcher.init(configFile, argumentsFile)
+}
